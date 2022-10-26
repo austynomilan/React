40778dc821a95afaa18dd65d52fbd7b71b76480c
Augustine Odoh O <62001398+austynomilan@users.noreply.github.com>
2022-10-26 02:39:14 +0100

    setting up commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
+const name = 'Austine'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <h1>Hello world</h1>
+     <h2>hello {1 + 1}</h2>
     </div>
   );
 }
